3ccb47769827a91562095af62dc77181dd7c29c0
Ondřej Svoboda <ondrej@svobodasoft.cz>
2017-01-15 20:54:45 +0100

    autopep8: style fixes in 'tools'

--- tools/create-lyricsource.py
+++ tools/create-lyricsource.py
@@ -94,6 +94,7 @@ if __name__ == '__main__':
     ${name}._app.run()
 """
 
+
 def input_name():
     prompt = 'Input the lyric source name with only lower-case alphabets and numbers:\n'
     while True:
@@ -103,6 +104,7 @@ def input_name():
         else:
             break
     return name
+
 
 def input_boolean(prompt, default_value):
     prompt += ' [Y/n]?' if default_value == True else ' [y/N]'
@@ -114,16 +116,19 @@ def input_boolean(prompt, default_value):
     else:
         return default_value == True
 
+
 def create_file(template, path, name, params):
     content = string.Template(template).substitute(params)
     f = open(os.path.join(path, name), 'w')
     f.write(content)
     f.close()
 
+
 def main():
     name = input_name()
     have_am = input_boolean('Generate Makefile.am', True)
-    have_subdir = input_boolean('Create source files in src subdirectory', False)
+    have_subdir = input_boolean(
+        'Create source files in src subdirectory', False)
     rootpath = name
     srcpath = name if not have_subdir else name + '/src'
     if not os.path.isdir(srcpath):
@@ -131,9 +136,10 @@ def main():
     params = {
         'name': name,
         'capsname': name.capitalize()
-        }
+    }
     create_file(PYTHON, srcpath, name + '.py', params)
-    create_file(SERVICE, srcpath, 'org.osdlyrics.LyricSourcePlugin.' + name + '.service.in', params)
+    create_file(SERVICE, srcpath, 'org.osdlyrics.LyricSourcePlugin.' +
+                name + '.service.in', params)
     if have_am:
         create_file(MAKEFILEAM, srcpath, 'Makefile.am', params)
         if have_subdir:
